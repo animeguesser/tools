02f8625b264edae6744b219044febdb1d6f111b5
Daniel Tam <daniel@danieltam.net>
2023-08-08 20:58:37 -0500

    add manual matching

--- parse-anime/match-mal-offlinedb.py
+++ parse-anime/match-mal-offlinedb.py
@@ -12,6 +12,18 @@ anime_db = json.load(f)
 matched_list = {'data': []}
 unmatched_list = []
 
+# Manual match (use the offline database name)
+manual_match = [
+    {
+        'mal_match': 'Tennis no Oujisama',
+        'db_match': 'Tennis no Ouji-sama'
+    },
+    {
+        'mal_match': 'Mukamuka Paradise',
+        'db_match': 'Muka Muka Paradise'
+    }
+]
+
 # Match the title from the MAL API to the offline DB
 for mal in mal_anime:
     found = False
@@ -32,6 +44,27 @@ for mal in mal_anime:
     # Create an list of unmatched titles
     if found == False:
         unmatched_list.append(mal['title'])
+
+# Manual matching
+for manual in manual_match:
+
+    # find entry in mal list
+    for mal in mal_anime:
+        if manual['mal_match'] == mal['title']:
+            break
+
+    # find entry in offline db
+    for anime in anime_db['data']:
+        if manual['db_match'] == anime['title']:
+            anime['mal_id'] = mal['id']
+
+            try:
+                anime['en'] = mal['en']
+            except KeyError:
+                pass
+
+            matched_list['data'].append(anime)
+            break
 
 # Write to disk the matched titles
 with open('matched-anime-list.json', 'w') as f:
