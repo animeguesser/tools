d4d58fdccc32fc90762c750c9ea5ee9ed5de4fd8
Daniel Tam <omgdnaieltam@gmail.com>
2023-10-30 16:13:38 +0000

    get mal list by popularity instead

--- parse-anime/get-mal.py
+++ parse-anime/get-mal.py
@@ -15,10 +15,10 @@ offset = 0
 anime_list = []
 
 # Get list of TV animes using MAL API
-while offset < 5000:
+while offset < 8000:
 
     # Can only get 500 at a time
-    url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=tv&fields=id,title,alternative_titles&limit=500&offset={offset}'
+    url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&fields=id,title,alternative_titles&limit=500&offset={offset}'
     resp = requests.get(url, headers={
         'X-MAL-CLIENT-ID': CLIENT_ID
     })
@@ -29,48 +29,6 @@ while offset < 5000:
         anime_list.append({'title': node['node']['title'], 'id': node['node']['id'], 'en': node['node']['alternative_titles']['en']})
 
     # Start at the next 500
-    offset = offset + 500
-
-    # Let's not spam the MAL API
-    time.sleep(0.5)
-
-# Get the list of movie animes using MAL API
-offset = 0
-while offset < 1000:
-    
-    # Limit to 250 entries at a time
-    url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=movie&fields=id,title,alternative_titles&limit=500&offset={offset}'
-    resp = requests.get(url, headers={
-        'X-MAL-CLIENT-ID': CLIENT_ID
-    })
-    anime = resp.json()
-
-    # Add into our list
-    for node in anime['data']:
-        anime_list.append({'title': node['node']['title'], 'id': node['node']['id'], 'en': node['node']['alternative_titles']['en']})
-
-    # Start at the next 500
-    offset = offset + 500
-
-    # Let's not spam the MAL API
-    time.sleep(0.5)
-
-# Get the list of ova animes using MAL API
-offset = 0
-while offset < 500:
-    
-    # Limit to 250 entries at a time
-    url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=ova&fields=id,title,alternative_titles&limit=500&offset={offset}'
-    resp = requests.get(url, headers={
-        'X-MAL-CLIENT-ID': CLIENT_ID
-    })
-    anime = resp.json()
-
-    # Add into our list
-    for node in anime['data']:
-        anime_list.append({'title': node['node']['title'], 'id': node['node']['id'], 'en': node['node']['alternative_titles']['en']})
-
-    # Start at the next 250
     offset = offset + 500
 
     # Let's not spam the MAL API
